f7c10c029bf08ebdd29b86314b05a287016df0bf
yuanwenhui <yuanwenhui93@163.com>
2021-07-28 11:47:32 +0800

    feat: 选择排序

---
+++ selectSort.js
@@ -0,0 +1,28 @@
+/**
+ * 选择排序，每次循环，确定最小的元素，并将元素交换到首位置
+ * @param array
+ * @returns {*}
+ */
+function selectSort(array) {
+  let length = array.length;
+  if (!Array.isArray(arr) || length < 1) return;
+  for (i = 0; i < length; i++) {
+    let minIndex = i;
+    for ( let j = i + 1; j <length; j++) {
+      if (array[minIndex] > array[j]) {
+        minIndex = j;
+      }
+    }
+    swap(array, i , minIndex);
+  }
+  return array;
+}
+
+function swap(array, left, right) {
+  var temp = array[left];
+  array[left] = array[right]
+  array[right] = temp;
+}
+
+const arr = [1,9,4,2,4,6,1,51]
+console.log(selectSort(arr))
