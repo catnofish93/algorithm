30fc4ba3ee6eddf74010bd35e09a674eb704c7ff
yuanwenhui <yuanwenhui93@163.com>
2021-07-28 10:04:04 +0800

    feat: 冒泡排序

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,6 +1,12 @@
+/**
+ * 冒泡排序，最好的时间复杂度为O(n), 最坏时间复杂度为O(n2), 空间复杂度为O(1)
+ * @param arr
+ * @returns {*}
+ */
 function bubbleSort(arr) {
   if (!Array.isArray(arr) || arr.length <= 1) return;
   let lastIndex = arr.length - 1;
+  // 当最后一个交换元素为第一个时，说明已排序完毕
   while (lastIndex > 0) {
     let flag = true, k = lastIndex;
     for (let j = 0; j< k; j++) {
