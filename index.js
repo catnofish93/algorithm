1a444e9458f4cba1101bea34f25f827de3b3a50f
yuanwenhui <yuanwenhui93@163.com>
2021-09-10 13:39:50 +0800

    feat: shell

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(['1', '2', '3'].map(parseInt))
+function cssStyle2DomStyle(sName) {
+  let arr = sName.split('-')
+  arr = arr.filter(item => {
+    return item !== ''
+  }).map((item, index) => {
+    if (index !== 0) {
+      item = item.substring(0, 1).toUpperCase() + item.substring(1)
+    }
+    return item
+  })
+  return arr.join('')
+}
+console.log(cssStyle2DomStyle('webkitBackgroundComposite'))
